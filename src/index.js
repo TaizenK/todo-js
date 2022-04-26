fbf98a9caff1a4417e9c6fdc62281ec366d2f63a
TaizenK <99730698+TaizenK@users.noreply.github.com>
2022-04-26 14:19:21 +0900

    ３４まで終了

--- src/index.js
+++ src/index.js
@@ -15,9 +15,26 @@ const onClickAdd = () => {
   // liの間に入力文字を入れる
   li.innerText = inputText;
 
+  // button（完了）タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button（削除）タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
